9067d2298c6d8e9442911fee96ef1d644ad7b697
KingsleyChung <kingsleychung@outlook.com>
2017-12-22 20:14:09 +0800

    Add map

---
+++ client/app/src/main/java/cn/kingsleychung/final_project/TaskFragment.java
@@ -0,0 +1,20 @@
+package cn.kingsleychung.final_project;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kings on 2017/12/15.
+ */
+
+public class TaskFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.task, container, false);
+    }
+}
