d36a904ca1ce89d8dd2908ddb50a239da2e0c4c5
weimumu <934657014@qq.com>
2018-01-07 20:53:40 +0800

    finish logout Button

--- client/app/src/main/java/cn/kingsleychung/final_project/ProfileFragment.java
+++ client/app/src/main/java/cn/kingsleychung/final_project/ProfileFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,10 +18,12 @@ public class ProfileFragment extends Fragment{
     private View ProfileView;
     private ImageView imageView;
     private TextView textView;
+    private Button logoutButton;
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         ProfileView = inflater.inflate(R.layout.activity_profile_fragment, container, false);
         imageView = ProfileView.findViewById(R.id.profilePhoto);
         textView = ProfileView.findViewById(R.id.profileName);
+        logoutButton = ProfileView.findViewById(R.id.logoutButton);
         String username = UserManagement.getInstance().getUser().getUserName() + ".jpg";
         textView.setText(UserManagement.getInstance().getUser().getUserName());
         UserManagement.getInstance().getPhoto(username, new Subscriber<Bitmap>() {
@@ -37,6 +40,13 @@ public class ProfileFragment extends Fragment{
             @Override
             public void onNext(Bitmap bitmap) {
                 imageView.setImageBitmap(bitmap);
+            }
+        });
+        // TODO with logout
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
         return ProfileView;
